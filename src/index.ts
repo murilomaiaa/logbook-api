cf39eef8a4de7be314d625104108f5d35e788b95
Murilo Maia <murilomaiab@gmail.com>
2023-08-25 21:43:23 -0300

    refactor: move config to main module

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
+import config from './main/config'
 import { Server } from './Server'
 import { MongoHelper } from './infra/mongodb/MongoHelper'
-import config from 'config'
 
 const server = new Server()
 
 MongoHelper.connect(config.mongoUrl).then(() => {
-  server.app.listen(3000, () => console.log('Server running'))
+  server.app.listen(config.port, () => console.log(`Server running on port ${config.port}`))
 })
--- src/main/config.ts
+++ src/main/config.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 
 const config = {
   mongoUrl: process.env.MONGO_URL || '',
+  port: process.env.PORT || 3000,
 }
 
 export default config
